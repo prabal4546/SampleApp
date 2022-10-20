7d85f18693e5bb49c2d86ce5e6c2d9f4db61861f
Prabaljit Walia <prabaljitwalia4546@gmail.com>
2022-10-20 22:37:13 +0530

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Prabaljit Walia on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
