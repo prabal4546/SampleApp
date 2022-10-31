423f29f1b36b0b74f5c17739a950eee54c2e68ab
Iqrah Nadeem <iiqrah.dev@gmail.com>
2022-10-31 11:58:27 +0300

    Create Navigation Between NFT Card and NFT Detail View

---
+++ SampleApp/View/NFTDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  NFTDetailView.swift
+//  SampleApp
+//
+//  Created by Iqrah Nadeem on 31/10/2022.
+//
+
+import SwiftUI
+
+struct NFTDetailView: View {
+    var body: some View {
+        
+        // Temporary, will be replaced with the referenced NFT Detail View UI
+        Text("NFT Detail View")
+    }
+}
+
+struct NFTDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NFTDetailView()
+    }
+}
